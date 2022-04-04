617b479d3f56a22592d92ab403ebcd4f3bf3a10b
tarun00710 <tarunlal07@gmail.com>
2022-04-04 18:35:19 +0530

    minor style changes

--- src/Homepage.js
+++ src/Homepage.js
@@ -7,7 +7,7 @@ const Homepage = () => {
   return (
     <div className='heading'>
     {image ? <img className='img' src={images} alt='any'/> : 
-    <div>
+    <div className='heading_middle'>
       <Link to="/class5/days">
         <button className='button1'> Class 1 - 5  </button>
       </Link>
@@ -15,7 +15,7 @@ const Homepage = () => {
           <button className='button1'> Class 6 - 8 </button>
       </Link>
      </div>}
-      <button className='button1' onClick={()=> setImage(!image)}>{image ? "Hide Rate" : "View Rate"}</button>
+      <button className='button3' onClick={()=> setImage(!image)}>{image ? "Hide Rate" : "View Rate"}</button>
       <Footer/>
       </div>
   )
